14497f19cd3c2018c32c1ef2865f8598a65456ed
Noah Tjoen <n.j.tjoen@student.tudelft.nl>
2022-11-22 15:40:38 +0100

    Start with side menu for small devices

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from "next/link";
 import Image from "next/image"
-import {AiOutlineMenu} from "react-icons/ai";
+import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai";
 
 
 const Navbar = () => {
@@ -26,6 +26,19 @@ const Navbar = () => {
                     </ul>
                     <div className='md:hidden'>
                         <AiOutlineMenu size={25}/>
+                    </div>
+                </div>
+            </div>
+            <div className='left-0 top-0 fixed bg-black/60 h-screen w-full'>
+                <div
+                    className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] bg-[#ecf0f3] h-screen ease-in duration-500'>
+                    <div>
+                        <div className='flex w-full items-center py-7 px-7 justify-between'>
+                            <Image src='/../public/Kitty.png' alt='/' width='50' height='50'/>
+                            <div>
+                                <AiOutlineClose/>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
